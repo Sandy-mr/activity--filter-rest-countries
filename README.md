05c13e483e07312362e85c515a26a0740e86d37c
Sandy-mr <sandy.mulato.r@gmail.com>
2018-08-02 11:32:18 -0500

    warmup

---
+++ README.md
@@ -0,0 +1,43 @@
+# Promises + REST Countries
+
+## The Assignment
+
+Use a `Promise` library to request data from the Rest Countries API and render an array of JSON data to the page.
+For each element, should render the “name”, “latitud”, “longitud”, and “flag”.
+
+## Normal Mode
+
+### Mockup
+
+![Demo](/mockup.png)
+
+> You can play with the demo, [here](https://muktekguest.github.io/demo--filter-countries/).
+
+### Deliverables
+
+1. An application that uses superagent Promise library to fetch JSON data from the GitHub REST API.
+2. JSON rendered to the page as HTML
+3. A GitHub repository named activity--filter-rest-countries
+
+### Setup Instructions
+
+**1. Create the assignment folder in your `~/muktek/assignments` directory and `cd` into it.**
+
+```sh
+~/muktek/assignments $: mkdir activity--filter-rest-countries
+~/muktek/assignments $: cd activity--filter-rest-countries
+```
+
+**2. In index.html use the `<script>` tag to link to the superagent library and link to your javascrpt file (./js/app.js).**
+
+**3. Make sure you load the `<script>` with superagent.**
+
+*You can find a link to the superagent library here: https://cdnjs.cloudflare.com/ajax/libs/superagent/3.6.0/superagent.min.js*
+
+**4. Make sure the superagent library is linked properly.**
+
+---
+
+## Keys to success
+
+**Read**.
---
+++ js/app.js
@@ -0,0 +1,39 @@
+request = superagent;
+countries = "https://restcountries.eu/rest/v2/all";
+
+document.querySelectorAll(".country").forEach(function (button) {
+  button.addEventListener('click', function (event) { 
+    var languages = event.target.textContent.toLowerCase();
+    console.log(languages)
+    var count = 0;
+    
+    request.get(countries).then(function (content) {
+        const countries = content.body;
+        var html = "";
+        countries.forEach(function (country) {
+          console.log(country)         
+          if (languages === country.languages[0].iso639_1) {
+            count++;
+            html += '<tr>';
+              html += '<td>'+ country.name+'</td>'
+              html +='<td>'+ country.latlng[0]+'</td>';
+              html +='<td>'+country.latlng[1]+'</td>';
+              html +='<td><img src="'+ country.flag+'" class="country-flag" /></td>';
+            html += ' </tr>';
+          }
+          if (languages == "all") {
+            count++;
+            html += "";
+              html += '<tr>';
+                html += '<td>'+ country.name +'</td>'
+                html += '<td>'+ country.latlng[0]+'</td>'
+                html += '<td>'+ country.latlng[1]+'</td>'
+                html +='<td><img src="'+country.flag +'" class="country-flag" /></td>';
+              html += ' </tr>';
+          }
+        });
+          document.querySelector("#country-number").textContent = count;
+         document.querySelector("table tbody").innerHTML = html;
+      })
+  });
+});
